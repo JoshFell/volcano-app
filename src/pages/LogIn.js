34330721d62e93554f659bd1a22b8d2fc86dc17f
Yoah98 <joshfell98@gmail.com>
2022-05-21 03:15:16 +1000

    Update register and login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -5,16 +5,8 @@ function Login() {
   const API_URL = `http://sefdb02.qut.edu.au:3001`
   const token = localStorage.getItem("token")
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-
-  function GetEmail(foundEmail){
-    setEmail(foundEmail);
-  }
-
-  function GetPassword(foundPassword){
-    setPassword(foundPassword);
-  }
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   function UserLogin(){
       const url = `${API_URL}/user/login`;
@@ -22,14 +14,13 @@ function Login() {
       return fetch(url, {
       method: "POST",
       headers: {accept: "application/json", "Content-Type" : "application/json"},
-      // body: JSON.stringify({email: `${email}`, password: `${password}`}) 
-      body: JSON.stringify({email: "mike@gmail.com", password: "password"})
+      body: JSON.stringify({email: `${email}`, password: `${password}`})
     })
     .then(res => res.json())
     .then(res => {res.message ? alert(res.message) : alert("Login Successful")}) //console.log(res)
     .then(res => {
       localStorage.setItem("token", res.token); //possible help to use token: https://www.freecodecamp.org/news/how-to-use-localstorage-with-react-hooks-to-set-and-get-items/
-    });
+    })
   }
 
   return (
@@ -38,20 +29,12 @@ function Login() {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            // onChange={GetEmail(email)}
-          />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            // onChange={GetPassword(password)} 
-          />
+          <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
         <Button variant="danger" onClick={UserLogin}>
           Login
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap'
 
 function Register() {
   const API_URL = `http://sefdb02.qut.edu.au:3001`
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   function RegisterUser(){
     const url = `${API_URL}/user/register`;
@@ -10,7 +12,7 @@ function Register() {
     return fetch(url, {
     method: "POST",
     headers: {accept: "application/json", "Content-Type" : "application/json"},
-    body: JSON.stringify({email: "fgasfgadf@gmail.com", password: ""}) 
+    body: JSON.stringify({email: `${email}`, password: `${password}`})
   })
   .then(res => res.json())
   .then(res =>alert(res.message))
@@ -22,12 +24,12 @@ function Register() {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
         <Button variant="danger" onClick={RegisterUser}>
           Register
